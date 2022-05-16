f19f43de4a5b8572cc057e394e27271723d79e28
micuks <sdkjwql@gmail.com>
2022-05-16 22:07:23 +0800

    defined class History

---
+++ src/History.h
@@ -0,0 +1,73 @@
+#ifndef HISTORY_H
+#define HISTORY_H
+
+#include <bits/stdc++.h>
+using namespace std;
+
+class BaseHistory {
+protected:
+    string hid, pid, sid, rid, pname, sname, rname, state, stime, rtime;
+public:
+    BaseHistory() {}
+    BaseHistory(string _hid, string _pid, string _sid, string _rid, string _pname, string _sname, string _rname, string _state = "none", string _stime = "none", string _rtime = "none") : hid(_hid), pid(_pid), sid(_sid), rid(_rid), pname(_pname), sname(_sname), rname(_rname), state(_state), stime(_stime), rtime(_rtime) {}
+    string getHid() const { return hid; }
+    string getPid() const { return pid; }
+    string getSid() const { return sid; }
+    string getRid() const { return rid; }
+    string getPname() const { return pname; }
+    string getSname() const { return sname; }
+    string getRname() const { return rname; }
+    string getState() const { return state; }
+    string getStime() const { return stime; }
+    string getRtime() const { return rtime; }
+    bool match(const string &s) const;
+    void print() const;
+    friend istream &operator>>(istream &in, BaseHistory &bh);
+    friend ostream &operator<<(ostream &out, const BaseHistory &bh);
+};
+
+class History: public BaseHistory {
+    class Record {
+    public:
+        Record(){}
+        Record(string _time, string _state):time(_time), state(_state){}
+        friend istream &operator >> (istream &in, Record &r);
+        friend ostream &operator << (ostream &out, const Record &r);
+    private:
+        string time, state;
+    };
+    vector <Record> record;
+public:
+    using BaseHistory::BaseHistory;
+    BaseHistory getBase() const { return BaseHistory(hid, pid, sid, rid, pname, sname, rname, state, stime, rtime); }
+    void reqSend();
+    void agrSend();
+    void canSend();
+    void refSend();
+    void reqRecv();
+    void agrRecv();
+    void canRecv();
+    void refRecv();
+    friend istream &operator >> (istream &in, Record &r);
+    friend ostream &operator << (ostream &out, const Record &r);
+    friend istream &operator >> (istream &in, History &h);
+    friend ostream &operator << (ostream &out, const History &h);
+};
+
+class HistoryList {
+    vector <BaseHistory> hl;
+public:
+    BaseHistory &operator [] (const string &hid);
+    BaseHistory &operator [] (const int &num);
+    int size() const { return hl.size(); }
+    void add(const BaseHistory &bh);
+    void del(const BaseHistory &bh);
+    void del(const string &hid);
+    int print() const; // 将HistoryList的信息打印, 返回其大小
+    string print(const string &hid) const; // 将HistoryList[hid]打印, 返回其History
+    void schHistory(const string &s) const;
+    friend istream &operator >> (istream &in, HistoryList &hl);
+    friend ostream &operator << (ostream &out, HistoryList &hl);
+};
+
+#endif
